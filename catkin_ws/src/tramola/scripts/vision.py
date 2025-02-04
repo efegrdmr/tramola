c759c2f8b14fd6c45f6de584949e26fb45ff2ffa
Efe Gürdamar <efegurdamar@outlook.com>
2025-02-04 17:37:47 +0300

    vision with tensorrt

--- catkin_ws/src/tramola/scripts/vision.py
+++ catkin_ws/src/tramola/scripts/vision.py
@@ -1,67 +1,127 @@
 #!/usr/bin/env python3
 
+'''
+git clone https://github.com/Tencent/ncnn.git
+cd ncnn/tools/darknet2onnx
+python3 darknet2onnx.py --cfg yolov4.cfg --weights yolov4.weights --output yolov4.onnx
+#onxx oluştu
+cd ..
+/tramola/src/tensorrt/bin/trtexec --onnx=yolov4.onnx --saveEngine=yolov4.trt --fp16
+#tensorrt tam konum bakilmali
+#trt oluştu
+'''
+
 import os
 import cv2
 import rospy
-from cv_bridge import CvBridge
-from sensor_msgs.msg import Image
+import numpy as np
+import tensorrt as trt
+import pycuda.driver as cuda
+import pycuda.autoinit
 from tramola.msg import Detection, DetectionList
-from ultralytics import YOLO
 
-def main():
-    rospy.init_node('yolo_detection_node', anonymous=True)
-    detection_pub = rospy.Publisher('yolo_detections', DetectionList, queue_size=10)
-    image_pub = rospy.Publisher('/camera/image_raw', Image, queue_size=10)
-    bridge = CvBridge()
-    rate = rospy.Rate(10)
+class YOLOv4TensorRT:
+    def __init__(self):
+        rospy.init_node('yolo_detection_node', anonymous=True)
+        self.publisher = rospy.Publisher('yolo_detections', DetectionList, queue_size=10)
+        self.rate = rospy.Rate(10)
 
-    MODEL_PATH = os.getenv('MODEL_PATH', 'path_to_your_model.pt')
-    model = YOLO(MODEL_PATH)
+        #TRT PATH DEGISTIR
+        self.MODEL_PATH = os.getenv('MODEL_PATH', 'path_to_yolov4.trt')
+        self.logger = trt.Logger(trt.Logger.WARNING)
+        self.runtime = trt.Runtime(self.logger)
 
-    cap = cv2.VideoCapture(0)
+        rospy.loginfo(f"Loading TensorRT model from {self.MODEL_PATH}")
+        with open(self.MODEL_PATH, "rb") as f:
+            self.engine = self.runtime.deserialize_cuda_engine(f.read())
 
-    while not rospy.is_shutdown():
-        ret, frame = cap.read()
-        if not ret:
-            continue
+        self.context = self.engine.create_execution_context()
+        self.inputs, self.outputs, self.bindings = self.allocate_buffers()
 
-        # Publish raw camera frame
-        image_pub.publish(bridge.cv2_to_imgmsg(frame, "bgr8"))
+        self.cap = cv2.VideoCapture(0)
 
-        # Run YOLO
-        results = model(frame)
+    def allocate_buffers(self):
+        """Allocates memory for TensorRT inference"""
+        inputs, outputs, bindings = [], [], []
+        for i in range(self.engine.num_bindings):
+            dtype = trt.nptype(self.engine.get_binding_dtype(i))
+            shape = self.engine.get_binding_shape(i)
+            size = trt.volume(shape) * np.dtype(dtype).itemsize
+            device_mem = cuda.mem_alloc(size)
 
-        # Build detection list
-        height, width, _ = frame.shape
-        detection_list = DetectionList()
-        for result in results:
-            for box in result.boxes:
-                x_min, y_min, x_max, y_max = box.xyxy[0].tolist()
-                confidence = box.conf[0].item()
-                class_id = int(box.cls[0].item())
+            if self.engine.binding_is_input(i):
+                inputs.append({"shape": shape, "dtype": dtype, "device": device_mem})
+            else:
+                outputs.append({"shape": shape, "dtype": dtype, "device": device_mem})
 
+            bindings.append(int(device_mem))
+
+        return inputs, outputs, bindings
+
+    def detect_objects(self, frame):
+        """Runs YOLOv4 inference using TensorRT"""
+        input_size = (416, 416)
+        blob = cv2.dnn.blobFromImage(frame, 1/255.0, input_size, swapRB=True, crop=False)
+
+        cuda.memcpy_htod(self.inputs[0]['device'], blob.ravel())
+
+        # Run inference
+        self.context.execute_v2(self.bindings)
+
+        # Retrieve and process output
+        output = np.empty(self.outputs[0]['shape'], dtype=self.outputs[0]['dtype'])
+        cuda.memcpy_dtoh(output, self.outputs[0]['device'])
+        return self.postprocess(output, frame.shape[:2])
+
+    def postprocess(self, output, img_shape):
+        """Decodes YOLOv4 output into detection format"""
+        boxes, confidences, class_ids = [], [], []
+        for detection in output:
+            x, y, w, h, conf, class_id = detection[:6]  # Assumed YOLO format
+            if conf > 0.5:
+                boxes.append([x, y, w, h])
+                confidences.append(conf)
+                class_ids.append(int(class_id))
+
+        # Apply Non-Maximum Suppression (NMS)
+        indices = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+        detections = []
+        for i in indices.flatten():
+            x, y, w, h = boxes[i]
+            detections.append([x, y, w, h, class_ids[i], confidences[i]])
+
+        return detections
+
+    def run(self):
+        """Main loop for capturing, detecting, and publishing detections"""
+        while not rospy.is_shutdown():
+            ret, frame = self.cap.read()
+            if not ret:
+                continue
+
+            detections = self.detect_objects(frame)
+
+            height, width, _ = frame.shape
+            detection_list = DetectionList()
+            for x, y, w, h, class_id, confidence in detections:
                 detection = Detection()
-                x_center = (x_min + x_max) / 2.0 / width
-                y_center = (y_min + y_max) / 2.0 / height
-                box_width = (x_max - x_min) / width
-                box_height = (y_max - y_min) / height
-
-                detection.x_center = x_center
-                detection.y_center = y_center
-                detection.width = box_width
-                detection.height = box_height
+                detection.x_center = (x + w / 2.0) / width
+                detection.y_center = (y + h / 2.0) / height
+                detection.width = w / width
+                detection.height = h / height
                 detection.confidence = confidence
                 detection.class_id = class_id
                 detection_list.detections.append(detection)
 
-        detection_pub.publish(detection_list)
-        rospy.loginfo("Published {} detections.".format(len(detection_list.detections)))
-        rate.sleep()
+            self.publisher.publish(detection_list)
+            rospy.loginfo("Published {} detections.".format(len(detection_list.detections)))
+            self.rate.sleep()
 
-    cap.release()
+        self.cap.release()
 
 if __name__ == '__main__':
     try:
-        main()
+        detector = YOLOv4TensorRT()
+        detector.run()
     except rospy.ROSInterruptException:
         pass
