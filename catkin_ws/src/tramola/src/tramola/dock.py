242bbb8a0c08a8d475fa5408b7162acd51a49132
Fatmademrr <fatma.demrr00@gmail.com>
2025-02-19 15:50:46 +0300

    Update dock.py

--- catkin_ws/src/tramola/src/tramola/dock.py
+++ catkin_ws/src/tramola/src/tramola/dock.py
@@ -2,8 +2,7 @@ from tramola.task import Task
 import rospy
 import time
 import math
-#import utm
-from tramola.sensor import get_sensor_distance
+from sensor import get_sensor_distance
 
 # açıları sabit kullandım onları dinamik hale getirmem lazım. kod çalıştığı durumda.
 class Docking(Task):
@@ -16,32 +15,10 @@ class Docking(Task):
 
     
     def find_dock(self, msg):
-        for detection in msg.detections:
-            self.sensor_distance = get_sensor_distance()
-            self.screen_center = 0.5
 
-            dets = [d for d in self.detections if d.class_id < 12]
-            
-            
-            if detection.class_id == self.objects["red_circle"]:
-                self.chosen_shape_id = detection
-            
-           
-            try:
-                self.sensor_distance = get_sensor_distance()
-                if self.sensor_distance is None or self.sensor_distance == 0:
-                    rospy.logwarn("Sensor distance is None or 0. Skipping this detection.")
-                    continue  # Mesafe geçerli değilse, bu tespiti atla
-            except Exception as e:
-                rospy.logerr(f"Error getting sensor distance: {e}")
-                continue
+        dets = [d for d in self.detections if d.class_id < 12]
 
-                
-            if detection.confidence < 0.3:
-                    rospy.logwarn(f"Low confidence: {detection.confidence}. Ignoring detection.")
-                    continue
-                     
-            if len(dets) == 0:
+        if len(dets) == 0:
                 self.vehicle.go_straight()
                 rospy.sleep(5)
                 if len(dets) == 0:
@@ -53,86 +30,65 @@ class Docking(Task):
                         self.vehicle.go_straight()
                         rospy.sleep(2)
 
-                    else:
-                        if detection.self.chosen_shape_id  is not None:
-                            if  abs(self.offset) < 0.1: 
-                                while self.sensor_distance > 125:
-                                    self.vehicle.go_straight()
-                                    if self.sensor_distance == 125:
-                                        self.vehicle.stop()
-                                        if self.offset < -0.1:
-                                            rospy.loginfo("The object is to the right, and the vehicle turns to the right.")
-                                            self.vehicle.turn_degrees(5)
-                                            while self.sensor_distance > 125:
-                                                self.vehicle.go_straight()
-                                                if self.sensor_distance == 125:
-                                                    self.vehicle.stop()
-                                                    return True
-                                        elif self.offset > 0.1:
-                                            rospy.loginfo("The object is to the left, and the vehicle turns to the left.")
-                                            self.vehicle.turn_degrees(-5)
-                                            while self.sensor_distance > 125:
-                                                self.vehicle.go_straight()
-                                                if self.sensor_distance == 125:
-                                                    self.vehicle.stop()
-                                                    return True
-                else:
-                    if detection.self.chosen_shape_id is not None:
-                        if  abs(self.offset) < 0.1: 
-                            while self.sensor_distance > 125:
-                                self.vehicle.go_straight()
-                                if self.sensor_distance == 125:
-                                    self.vehicle.stop()
-                                    if self.offset < -0.1:
-                                        rospy.loginfo("The object is to the right, and the vehicle turns to the right.")
-                                        self.vehicle.turn_degrees(5)
-                                        while self.sensor_distance > 125:
-                                            self.vehicle.go_straight()
-                                            if self.sensor_distance == 125:
-                                                self.vehicle.stop()
-                                                return True
-                                    elif self.offset > 0.1:
-                                        rospy.loginfo("The object is to the left, and the vehicle turns to the left.")
-                                        self.vehicle.turn_degrees(-5)
-                                        while self.sensor_distance > 125:
-                                            self.vehicle.go_straight()
-                                            if self.sensor_distance == 125:
-                                                self.vehicle.stop()
-                                                return True
-            else:
-                if detection.self.chosen_shape_id is not None:
+
+        if len(dets) != 0:
+            for detection in msg.detections:
+                self.sensor_distance = get_sensor_distance()
+                self.screen_center = 0.5
+                self.offset = self.screen_center - self.chosen_shape.x_center
+
+                
+                
+                if detection.class_id == self.objects["red_circle"]:
+                    self.chosen_shape = detection
+            
+                try:
+                    self.sensor_distance = get_sensor_distance()
+                    if self.sensor_distance is None or self.sensor_distance == 0:
+                        rospy.logwarn("Sensor distance is None or 0. Skipping this detection.")
+                        continue  # Mesafe geçerli değilse, bu tespiti atla
+                except Exception as e:
+                    rospy.logerr(f"Error getting sensor distance: {e}")
+                    continue
+
+                    
+                if detection.confidence < 0.3:
+                        rospy.logwarn(f"Low confidence: {detection.confidence}. Ignoring detection.")
+                        continue
+                        
+                if detection.class_id == self.chosen_shape:
                     if  abs(self.offset) < 0.1: 
                         while self.sensor_distance > 125:
                             self.vehicle.go_straight()
                             if self.sensor_distance == 125:
                                 self.vehicle.stop()
-                                if self.offset < -0.1:
-                                    rospy.loginfo("The object is to the right, and the vehicle turns to the right.")
-                                    self.vehicle.turn_degrees(5)
-                                    while self.sensor_distance > 125:
-                                        self.vehicle.go_straight()
-                                        if self.sensor_distance == 125:
-                                            self.vehicle.stop()
-                                            return True
-                                elif self.offset > 0.1:
-                                    rospy.loginfo("The object is to the left, and the vehicle turns to the left.")
-                                    self.vehicle.turn_degrees(-5)
-                                    while self.sensor_distance > 125:
-                                        self.vehicle.go_straight()
-                                        if self.sensor_distance == 125:
-                                            self.vehicle.stop()
-                                            return True
-                                        
+                    if self.offset < -0.1:
+                        rospy.loginfo("The object is to the right, and the vehicle turns to the right.")
+                        self.vehicle.turn_degrees(5)
+                        while self.sensor_distance > 125:
+                            self.vehicle.go_straight()
+                            if self.sensor_distance == 125:
+                                self.vehicle.stop()
+                                return True
+                    if self.offset > 0.1:
+                        rospy.loginfo("The object is to the left, and the vehicle turns to the left.")
+                        self.vehicle.turn_degrees(-5)
+                        while self.sensor_distance > 125:
+                            self.vehicle.go_straight()
+                            if self.sensor_distance == 125:
+                                self.vehicle.stop()
+                                return True
+                
 
     def detection_callback(self, msg):
         # aracın limana yanaşmasını sağlamak için.
         for detection in msg.detections:
             self.sensor_distance = get_sensor_distance()
             self.screen_center = 0.5
-            self.offset = self.screen_center - self.chosen_shape_id.x_center 
+            self.offset = self.screen_center - self.chosen_shape.x_center 
 
             if detection.class_id == self.objects["red_circle"]:
-                self.chosen_shape_id = detection
+                self.chosen_shape = detection
             
             
             if self.find_dock(self,msg): # bakcam tekrar.
@@ -151,11 +107,11 @@ class Docking(Task):
                     self.vehicle.go_straight()
                     rospy.sleep(3)
                     self.vehicle.turn_degrees(160)
-                    if detection.self.chosen_shape_id is None:
+                    if detection.class_id != self.chosen_shape:
                         self.vehicle.turn_degrees(-20)
-                        if detection.self.chosen_shape_id is None:
+                        if detection.class_id != self.chosen_shape:
                             self.vehicle.turn_degrees(40)
-                            if detection.self.chosen_shape_id  is not None:
+                            if detection.class_id == self.chosen_shape:
                                 self.vehicle.go_straight()
                                 rospy.sleep(2)
                                 self.vehicle.turn_degrees(-20)
@@ -174,16 +130,14 @@ class Docking(Task):
                                             self.vehicle.stop()
                                             
 
-                                elif self.offset > 0.1:
+                                if self.offset > 0.1:
                                     rospy.loginfo("The object is to the left, and the vehicle turns to the left.")
                                     self.vehicle.turn_degrees(-5)
                                     while self.sensor_distance > 22:
                                         self.vehicle.go_straight()
                                         if self.sensor_distance == 22:
                                             self.vehicle.stop()
-                        
-                                
-                        
+                                               
                         else:
                             self.vehicle.go_straight()
                             rospy.sleep(2)
@@ -203,7 +157,7 @@ class Docking(Task):
                                         self.vehicle.stop()
                                         
 
-                            elif self.offset > 0.1:
+                            if self.offset > 0.1:
                                 rospy.loginfo("The object is to the left, and the vehicle turns to the left.")
                                 self.vehicle.turn_degrees(-5)
                                 while self.sensor_distance > 22:
